0f69bd0879cc883f966c39143728bcff982b58ab
ange <angela.ebirim@fundingcircle.com>
2014-03-26 09:45:47 +0000

    rspec test

--- spec/pair_spec.rb
+++ spec/pair_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 require './autopair'
 
 describe Pair do
+subject {Pair.new.generate}
 
- it "generates a random name from the given array" do
+ it "generates a random name from array to see if it exists in array" do
 
 
-subject {Pair.new}
- subject.generate.should == "Boris Vela"
+ subject {should match @names} 
  end
 end 
